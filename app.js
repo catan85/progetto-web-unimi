6eb6009ec52ffbab18088db2b5e47bcde881e008
catan85 <catan85@gmail.com>
2018-06-10 09:45:15 +0200

    modificato launcher, modificato prima view da caricare, supporto CORS

--- app.js
+++ app.js
@@ -9,14 +9,27 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// imposta il path delle view e definisce come motore delle view handlebars
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+// imposta la view principale da caricare all'avvio
+app.set('view options', { layout: 'main' });
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// abilita le chiamate CORS da tutti gli host
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+  next();
+});
+
+// definisce il percorso dei contenuti statici
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
