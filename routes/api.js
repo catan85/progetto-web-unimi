90ad92f5c52903d0e0bac58333dbe8454c7f9449
ancattaneo <ancattaneo@marzoli.it>
2018-06-22 17:09:30 +0200

    inserito lettura utenti da database mongodb

--- routes/api.js
+++ routes/api.js
@@ -1,35 +1,19 @@
 var express = require('express');
 var router = express.Router();
-
-
-
+var mongoose = require('mongoose');
 
 router.get('/charts',
   require('connect-ensure-login').ensureLoggedIn(),
   function(req, res){
    
-    //// -------------------------------------
-    //    Ok funziona.. ma sarebbe meglio fare una connessione e sfruttare i modelli..
-    //// ---------------------------------------
-    console.log("connecting..");
-    
-    var mongoose = require('mongoose');
-    var uri = 'mongodb://mongouser:mongouser123@ds247330.mlab.com:47330/unimi';
-    mongoose.connect(uri);
-    var db = mongoose.connection;
+    // preleva da mongoose il modello riferito allo schema che abbiamo definito
+    var Measures = mongoose.model('measures');
 
-    db.on('error', console.error.bind(console, 'connection error:'));
-
-    db.once('open', function callback() {
-
-      // preleva da mongoose il modello riferito allo schema che abbiamo definito
-      var Measures = mongoose.model('measures');
-
-      // lettura effetiva dei dati, filtrati per username, passato da passport
-      Measures.find({'username': req.user.username}).sort({'timestamp': -1}).limit(30).exec(function(err,data){
-        res.json(data);
-      });
+    // lettura effetiva dei dati, filtrati per username, passato da passport
+    Measures.find({'username': req.user.username}).sort({'timestamp': -1}).limit(30).exec(function(err,data){
+      res.json(data);
     });
+  
   });
 
 module.exports = router;
