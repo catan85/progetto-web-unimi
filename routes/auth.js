491c6633fda9289631cbe5d9048a226d47e2ede3
catan85 <catan85@gmail.com>
2018-06-23 15:31:15 +0200

    aggiunto pulsante register in form login con link alla pagina register

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+var mongoose = require('mongoose');
 
 // Metodo POST per il login (lanciato dal form della pagina login)
 router.post('/login', 
@@ -8,6 +9,16 @@ router.post('/login',
   function(req, res) {
     res.redirect('/');
   });
+
+
+router.post('/register', function(req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    var email = req.body.email;
+
+    res.redirect('/');
+    
+});
 
 // Metodo GET per il logout
 router.get('/logout',
